d5786579008cdba532aa24f19d38184d8c4aedfd
Ádran Farias Carnavale <adran.carnavale@gmail.com>
2024-07-16 00:10:06 -0300

    feat: implements otel tracer

---
+++ service_b/infra/opentelemetry/tracer.go
@@ -0,0 +1,51 @@
+package opentelemetry
+
+import (
+	"context"
+	"errors"
+	"os"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/zipkin"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.24.0"
+)
+
+func Init(ctx context.Context) (shutdown func(context.Context) error, err error) {
+	var shutdownFuncs []func(context.Context) error
+
+	shutdown = func(ctx context.Context) error {
+		var err error
+		for _, fn := range shutdownFuncs {
+			err = errors.Join(err, fn(ctx))
+		}
+		shutdownFuncs = nil
+		return err
+	}
+
+	handleErr := func(inErr error) {
+		err = errors.Join(inErr, shutdown(ctx))
+	}
+
+	exporter, err := zipkin.New(os.Getenv("ZIPKIN_URL"))
+	if err != nil {
+		handleErr(err)
+		return
+	}
+
+	batcher := sdktrace.NewBatchSpanProcessor(exporter)
+
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithSpanProcessor(batcher),
+		sdktrace.WithResource(resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceName("service_b"),
+		)),
+	)
+
+	shutdownFuncs = append(shutdownFuncs, tp.Shutdown)
+	otel.SetTracerProvider(tp)
+
+	return
+}
